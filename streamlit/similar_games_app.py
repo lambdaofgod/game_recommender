f9883af6933cee4516bba62629cd8c129b962ee3
Jakub Bartczuk <bartczukkuba@gmail.com>
2020-05-23 17:56:01 +0200

    interactive data using input in streamlit

--- streamlit/similar_games_app.py
+++ streamlit/similar_games_app.py
@@ -9,23 +9,21 @@ from sklearn import feature_extraction
 
 
 steam_df = steam_data.load_steam_df()
-vectorizer = steam_data.make_df_column_vectorizer([
-        ['popular_tags'],
-        ['game_description', 'desc_snippet'],
-    ],
-    vectorizer_classes=partial(
-            feature_extraction.text.CountVectorizer,
-            binary=True
-    ),
-    tokenizers=[steam_data.split_by_comma_tokenizer, None]
-)
-steam_metadata_vectors = vectorizer.fit_transform(steam_df)
+vectors_url = 'https://storage.googleapis.com/lambdastruck_bucket/datasets/steam/steam_metadata_reduced_vectors.csv.gz' 
+steam_metadata_vectors = pd.read_csv(vectors_url).values
 
-game = st.text_input('Input game name')
-if st.checkbox('Find similar games'):
-    st.text('Games matching title')
-    chosen_games_df = steam_data.get_games_by_name(steam_df, game)
-    st.write(chosen_games_df)
-    st.text('Similar games')
-    similar_games_df = steam_data.similar_games(chosen_games_df, steam_df, steam_metadata_vectors)
-    st.write(similar_games_df)
+
+def show_similar_games(game_name_substring):
+    chosen_games_df = steam_data.get_games_by_name(steam_df, game_name_substring)
+    if len(chosen_games_df) > 0:
+        similar_games_df = steam_data.similar_games(chosen_games_df, steam_df, steam_metadata_vectors)
+        chosen_games_df.index = chosen_games_df['name']
+        st.text('Games matching title')
+        st.write(chosen_games_df)
+        st.text('Similar games')
+        st.write(similar_games_df)
+    else:
+        st.text('Nothing found for query: "{}"'.format(game_name_substring))
+    
+game = st.text_input('Input game name', value='S.T.A.L.K.E.R')
+show_similar_games(game)
