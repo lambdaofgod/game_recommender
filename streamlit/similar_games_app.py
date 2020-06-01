2f739229cb7e60f8f94ba834d1d14eab2d5cbd08
Jakub Bartczuk <bartczukkuba@gmail.com>
2020-06-01 19:52:44 +0200

    data loading and content based recommender update

--- streamlit/similar_games_app.py
+++ streamlit/similar_games_app.py
@@ -10,12 +10,14 @@ steam_df_url = 'https://storage.googleapis.com/lambdastruck_bucket/datasets/stea
 steam_df = steam_data.load_steam_df(steam_df_url)
 vectors_url = 'https://storage.googleapis.com/lambdastruck_bucket/datasets/steam/steam_metadata_reduced_vectors.csv.gz' 
 steam_metadata_vectors = pd.read_csv(vectors_url).values
+searcher = content_based.MetadataSimilaritySearcher(steam_df, steam_metadata_vectors)
 
 
 def show_similar_games(game_name_substring):
     chosen_games_df = steam_data.get_games_by_name(steam_df, game_name_substring)
     if len(chosen_games_df) > 0:
-        similar_games_df = content_based.similar_games(chosen_games_df, steam_df, steam_metadata_vectors)
+        results = searcher.find_similar(chosen_games_df)
+        similar_games_df = content_based.make_stacked_results_df(*results)
         chosen_games_df.index = chosen_games_df['name']
         st.text('Games matching title')
         st.write(chosen_games_df)
