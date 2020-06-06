91085e3930a04478446368acb7cc7bf1841de8f0
Jakub Bartczuk <bartczukkuba@gmail.com>
2020-06-06 16:24:31 +0200

    streamlit update

--- streamlit/similar_games_app.py
+++ streamlit/similar_games_app.py
@@ -10,10 +10,11 @@ steam_df_url = 'https://storage.googleapis.com/lambdastruck_bucket/datasets/stea
 steam_df = steam_data.load_steam_df(steam_df_url)
 vectors_url = 'https://storage.googleapis.com/lambdastruck_bucket/datasets/steam/steam_metadata_reduced_vectors.csv.gz' 
 steam_metadata_vectors = pd.read_csv(vectors_url).values
-searcher = content_based.MetadataSimilaritySearcher(steam_df, steam_metadata_vectors)
+searcher = content_based.SimilaritySearcher(steam_df, steam_metadata_vectors)
 
 
-def show_similar_games(game_name_substring):
+def show_similar_games(raw_game_name_substring):
+    game_name_substring = steam_data.normalize_name(raw_game_name_substring)
     chosen_games_df = steam_data.get_games_by_name(steam_df, game_name_substring)
     if len(chosen_games_df) > 0:
         results = searcher.find_similar(chosen_games_df['name'])
